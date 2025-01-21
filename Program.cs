eaf3df7f11a8b86aac813241532aae5bf12c0ab4
Jean Bryant Figueroa <jbster559@gmail.com>
2025-01-21 10:12:34 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Author: Jean Bryant Figueroa
+//Course: COMP003A
+//Faculty: Johnathan Cruz
+//Purpose: A program demonstrating statements, varibles, and operators
+
+namespace COMP003A.LectureActivity2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
