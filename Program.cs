27a0b94238bc06e4db910eb963c133ac0101f7a9
Jean Bryant Figueroa <jbster559@gmail.com>
2025-01-21 18:50:08 -0800

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -9,7 +9,18 @@ namespace COMP003A.LectureActivity2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userName; //store name as a string
+            int userAge; //store age as an number
+            double productPrice; //store the product price
+            bool isStudent; //true / false statement
+
+            Console.Write("Enter your name: ");
+            userName = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            userAge = int.Parse(Console.ReadLine());
+
+            Console.Write
         }
     }
 }
